6d8281f9a8171f6f28d366febb103daca0d3ec7b
Carlo Hamalainen <carlo.hamalainen@gmail.com>
2013-02-11 21:54:24 +1100

    Commit of /usr/local/src directory which has CVL build config files.

---
+++ usr/local/src/MASSIVE-PREFERENCES/0.0.1/postinstall.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+rm -f /usr/lib/bonobo/servers/massivePreferencesApplet.server
+ln -s `find /usr/local/massive-preferences/ -iname '*.server'` /usr/lib/bonobo/servers/massivePreferencesApplet.server
+
+
+easy_install pip
+pip install ssh
+
+
+

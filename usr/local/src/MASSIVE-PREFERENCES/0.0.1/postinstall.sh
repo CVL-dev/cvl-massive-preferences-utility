831008fecd1d09a096f95f2653b645674e1dd161
Carlo Hamalainen <carlo.hamalainen@gmail.com>
2013-02-15 14:57:04 +1100

    Updating postinstall script.

--- usr/local/src/MASSIVE-PREFERENCES/0.0.1/postinstall.sh
+++ usr/local/src/MASSIVE-PREFERENCES/0.0.1/postinstall.sh
@@ -13,3 +13,10 @@ chmod a+rwx /home/projects
 
 echo "user_allow_other" > /etc/fuse.conf
 
+
+latest=`ls /usr/local/massive-preferences/ | sort | tail -1`
+
+cp -r /usr/local/massive-preferences/${latest}/massive-preferences/etc/skel/.config /etc/skel/
+
+
+

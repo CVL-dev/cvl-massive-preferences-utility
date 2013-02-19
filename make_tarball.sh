e2fbf918fc6377415e069bf3b1b1ee3a5eaecbe6
Carlo Hamalainen <carlo.hamalainen@gmail.com>
2013-02-19 11:26:48 +1100

    Standalone version.

--- make_tarball.sh
+++ make_tarball.sh
@@ -11,12 +11,13 @@ mkdir massive-preferences/lib
 mkdir massive-preferences/etc
 
 cp massivePreferencesApplet.py massive-preferences/bin/
-cp MASSIVElogoTransparent32x32.xpm massivePreferencesApplet.server massive-preferences/lib
+cp MASSIVElogoTransparent32x32.xpm massivePreferencesApplet.server Massive*.desktopmassive-preferences/lib
 cp -vr etc massive-preferences
 
 touch massive-preferences/readme.txt
 cd massive-preferences/lib
 sed -i "s/CVLMASSIVEPREFERENCESVERSION/${VERSION}/g" massivePreferencesApplet.server 
+sed -i "s/CVLMASSIVEPREFERENCESVERSION/${VERSION}/g" "Massive Preferences.desktop"
 cd -
 
 tar zcvf massive-preferences-${VERSION}.tar.gz massive-preferences
